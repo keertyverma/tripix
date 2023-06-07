f2d935bab3c5fce1d19a12692825ac226db4d821
Keerty Verma <keertyverma1303@gmail.com>
2023-06-07 10:03:59 +0530

    setup layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import { Box } from "@mui/material";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </Box>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Share, Connect & Explore. Share travel memories and Spread the joy."
+        />
+        <meta
+          property="og:title"
+          content="Tripix - Share, Connect & Explore. Share travel memories and Spread the joy."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="http://tripix.vercel.app/" />
+        <title>Tripix - Share Travel Memories</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
